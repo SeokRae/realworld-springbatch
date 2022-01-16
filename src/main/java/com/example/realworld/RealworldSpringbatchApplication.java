f765f6e8588c0c91a844cf526d3f9351dedd0dab
seokrae <kslbsh@gmail.com>
2022-01-16 23:02:44 +0900

    Initial commit

---
+++ src/main/java/com/example/realworld/RealworldSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldSpringbatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealworldSpringbatchApplication.class, args);
+    }
+
+}
